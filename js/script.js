59061123d2c96d18d764f2d97acf669e2d499231
AntonyLD <antonyldcamargo@gmail.com>
2024-05-16 15:21:47 -0300

    att button jutsu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ const buttons = document.querySelectorAll("#button-celector li button");
 
 const imgChange = document.querySelectorAll(".contianer-jutsus div img");
 
+const urlArchive = document.doctype.baseURI
+const splitUrl = urlArchive.split("/")
+const nameHtml = splitUrl[splitUrl.length -1]
+console.log(nameHtml)
+
+if(nameHtml === "index.html#"){
+  console.log("alo")
+}
+
+
+
+
 buttons.forEach((botao) => {
     botao.addEventListener("click", function()  {
       document.querySelectorAll("#nav-ul li").forEach(function(li) {
